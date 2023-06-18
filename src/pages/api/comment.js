4a28fb9b04ffe83ea255cf74676234713c319857
eeeunbiiii <121873609+eeeunbiiii@users.noreply.github.com>
2023-06-18 15:04:01 +0900

    final

--- src/pages/api/comment.js
+++ src/pages/api/comment.js
@@ -31,7 +31,7 @@ export default async function handler(req, res) {
           "https://api.openai.com/v1/completions",
           {
             prompt: promptComment,
-            max_tokens: 150,
+            max_tokens: 500,
             model: "text-davinci-003"
           },
           {
